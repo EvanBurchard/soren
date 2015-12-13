25b9b8ea73d302f1ff26beee15aef1140ef15c6c
evanburchard <evan.burchard@gmail.com>
2015-12-14 02:47:13 +0900

    broken with static and dynamic attempts at instance methods

--- soren.rb
+++ soren.rb
@@ -1,17 +1,13 @@
 module Soren
+  def nil_returner
+    if super().nil?
+      return NullObject
+    else
+      super
+    end
+  end
   def self.prepended(base)
     Object.const_set("NullObject", Class.new)
-    (base.instance_methods(false)).each do |method_name|
-      base.send(:define_method, method_name) do |*args|
-        return NullObject
-        super_method = base.send(methcod_name, args)
-        if(super_method.nil?)
-          return NullObject
-        else
-          super_method
-        end
-      end
-    end
     (base.methods - Object.methods).each do |method_name|
       base.class.class_exec do
         define_method(method_name) do
--- toy_class_test.rb
+++ toy_class_test.rb
@@ -4,6 +4,7 @@ require 'minitest/autorun'
 
 class TestSoren < MiniTest::Unit::TestCase
   def test_soren
+    assert(NullObject)
     assert_equal(NullObject, Toy.new.nil_returner )
     assert_equal(NullObject, Toy.class_nil_returner )
   end
