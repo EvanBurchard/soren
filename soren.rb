679bef005126acabd5d82a96a37a3633107286c0
evanburchard <evan.burchard@gmail.com>
2015-12-15 13:18:26 +0900

    reorganized

--- soren.rb
+++ soren.rb
@@ -1,7 +1,16 @@
 module Soren
-  def self.prepended(base)
-    Object.const_set("NullObject", Class.new)
 
+  def self.prepended(base)
+    create_null_object_class!
+    assign_instance_methods!(base)
+    assign_class_methods!(base)
+  end
+
+  def self.create_null_object_class!
+    Object.const_set("NullObject", Class.new)
+  end
+
+  def self.assign_instance_methods!(base)
     (base.instance_methods(false)).each do |method_name|
       old_method = base.instance_method(method_name)
       self.send(:define_method, method_name) do |*args, &block|
@@ -13,7 +22,9 @@ module Soren
         end
       end
     end
+  end
 
+  def self.assign_class_methods!(base)
     (base.methods - Object.methods).each do |method_name|
       old_method = base.method(method_name).unbind
       base.send(:define_singleton_method, method_name) do |*args, &block|
@@ -25,6 +36,6 @@ module Soren
         end
       end
     end
-
   end
+
 end
